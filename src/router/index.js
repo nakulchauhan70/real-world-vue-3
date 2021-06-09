6ad062a0b5a4129a10a76967864d111737ff5243
nchauh1 <Nakul.Chauhan@morningstar.com>
2021-06-09 10:53:23 +0530

    Nested routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import About from '@/views/About.vue'
-import EventList from '@/views/EventList.vue'
-import EventDetails from '@/views/event/Details.vue'
-import EventRegister from '@/views/event/Register.vue'
-import EventEdit from '@/views/event/Edit.vue'
+import About from '../views/About.vue'
+import EventList from '../views/EventList.vue'
+import EventLayout from '../views/event/Layout.vue'
+import EventDetails from '../views/event/Details.vue'
+import EventRegister from '../views/event/Register.vue'
+import EventEdit from '../views/event/Edit.vue'
 
 const routes = [
   {
@@ -14,21 +15,26 @@ const routes = [
   },
   {
     path: '/event/:id',
-    name: 'EventDetails',
+    name: 'EventLayout',
     props: true,
-    component: EventDetails
-  },
-  {
-    path: '/event/:id/register',
-    name: 'EventRegister',
-    props: true,
-    component: EventRegister
-  },
-  {
-    path: '/event/:id/edit',
-    name: 'EventEdit',
-    props: true,
-    component: EventEdit
+    component: EventLayout,
+    children: [  // <-----
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegister
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEdit
+      }
+    ]
   },
   {
     path: '/about',
